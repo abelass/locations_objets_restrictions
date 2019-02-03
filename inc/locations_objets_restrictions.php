6a750ab8d648b2d3f4530b1b556c7b612131b784
Rainer Müller <rainer@websimple.be>
2019-02-03 13:26:04 +0100

    Recupérer les données ders extension

---
+++ inc/locations_objets_restrictions.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION"))
+  return;
+
+/**
+ * Retourne les déclarations des champs inséré via la pipeline "prix_objets_extensions"
+ *
+ * @param array $valeurs
+ * @return array
+ */
+function locations_objets_restrictions_extensions_declaration($valeurs = array()) {
+
+  return pipeline(
+      'locations_objets_restrictions_extensions', array(
+        'data' => array(),
+        'args' => $valeurs,
+      )
+    );
+}
