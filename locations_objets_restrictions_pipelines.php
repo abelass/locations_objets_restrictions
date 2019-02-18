235ce73eb52e74d7131403cf9192f3dfa9bb9126
abelass <rainer@websimple.be>
2019-02-18 11:32:59 +0100

    utiliser le nouveau pipeline

--- locations_objets_restrictions_pipelines.php
+++ locations_objets_restrictions_pipelines.php
@@ -70,13 +70,13 @@ function locations_objets_restrictions_optimiser_base_disparus($flux) {
 /**
  * Compléter le tableau d’erreurs renvoyé par la fonction verifier d'un formulaire CVT.
  *
- * @pipeline formulaire_verifier
+ * @pipeline objets_location_verifier_dates
  * @param array $flux
  *  Données du pipeline
  * @return array
  *   Données du pipeline
  */
-function locations_objets_restrictions_formulaire_verifier($flux){
+function locations_objets_restrictions_objets_location_verifier_dates($flux){
 	if ($flux['args']['form'] == 'editer_objets_location'){
 		include_spip('inc/locations_objets_restrictions');
 		$flux['data'] = lor_verifier($flux['data']);
